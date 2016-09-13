3ddd7b207531396b5a59bd7a8c265fcde1453a64
Atsushi Nagase <a@ngs.io>
2016-09-13 23:10:45 +0900

    Command tests

--- app/commands.go
+++ app/commands.go
@@ -12,8 +12,8 @@ type Commands struct {
 }
 
 func (commands *Commands) SetBotID(botID string) {
-	commands.List = regexp.MustCompile(`\A<@` + botID + `>\s+list\s+containers?`)
+	commands.List = regexp.MustCompile(`\A<@` + botID + `>\s+(?:list|ls)(?:\s+containers?)?`)
 	commands.Start = regexp.MustCompile(`\A<@` + botID + `>\s+start\s+record(?:ing)?\s+(\S+)\s+(?:for\s+)?(\d+)\s*min(?:utes?)?`)
-	commands.Remove = regexp.MustCompile(`\A<@` + botID + `>\s+remove\s+(?:container\s+)?(\S+)`)
+	commands.Remove = regexp.MustCompile(`\A<@` + botID + `>\s+(?:remove|rm)\s+(?:container\s+)?(\S+)`)
 	commands.Ping = regexp.MustCompile(`\A<@` + botID + `>\s+ping`)
 }
--- app/commands_test.go
+++ app/commands_test.go
@@ -1,13 +1,37 @@
 package app
 
 import (
-	"fmt"
+	"regexp"
 	"testing"
 )
 
+func _TestCommands(t *testing.T, regexp *regexp.Regexp, commands []string) {
+	for _, cmd := range commands {
+		if !regexp.MatchString(cmd) {
+			t.Errorf("`%v` should match with `%v`, but not", cmd, regexp)
+		}
+	}
+}
+
 func TestBuildSetBotID(t *testing.T) {
-	app = &App{}
+	app := &App{}
 	app.SetBotID("rabot-test")
-  t.Fail()
-	fmt.Printf("%+v\n%+v\n%+v\n%+v\n", app.Commands.List, app.Commands.Start, app.Commands.Remove, app.Commands.Ping)
+	_TestCommands(t, app.Commands.List, []string{
+		"<@rabot-test>  list   container  ",
+		"<@rabot-test>  list   containers  ",
+		"<@rabot-test>  ls  containers ",
+		"<@rabot-test>  ls ",
+	})
+	_TestCommands(t, app.Commands.Start, []string{
+		"<@rabot-test>  start  record    ALPHA-STATION for  12 min ",
+		"<@rabot-test>  start  recording    ALPHA-STATION   1minute ",
+	})
+	_TestCommands(t, app.Commands.Remove, []string{
+		"<@rabot-test>  remove  container  foo  ",
+		"<@rabot-test>  rm  container  foo  ",
+		"<@rabot-test>  rm   foo  ",
+	})
+	_TestCommands(t, app.Commands.Ping, []string{
+		"<@rabot-test>  ping  ",
+	})
 }
