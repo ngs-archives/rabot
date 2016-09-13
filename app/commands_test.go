c71dfbd7df5d095fc1005bb9a949c936c41c7f11
Atsushi Nagase <a@ngs.io>
2016-09-14 06:35:48 +0900

    Use Commands instance in commands_Test

--- app/commands_test.go
+++ app/commands_test.go
@@ -14,9 +14,9 @@ func _TestCommands(t *testing.T, regexp *regexp.Regexp, commands []string) {
 }
 
 func TestListCommand(t *testing.T) {
-	app := &App{}
-	app.SetBotID("rabot-test")
-	_TestCommands(t, app.Commands.List, []string{
+	commands := &Commands{}
+	commands.SetBotID("rabot-test")
+	_TestCommands(t, commands.List, []string{
 		"<@rabot-test>  list   container  ",
 		"<@rabot-test>  list   containers  ",
 		"<@rabot-test>  ls  containers ",
@@ -25,18 +25,18 @@ func TestListCommand(t *testing.T) {
 }
 
 func TestStartCommand(t *testing.T) {
-	app := &App{}
-	app.SetBotID("rabot-test")
-	_TestCommands(t, app.Commands.Start, []string{
+	commands := &Commands{}
+	commands.SetBotID("rabot-test")
+	_TestCommands(t, commands.Start, []string{
 		"<@rabot-test>  start  record    ALPHA-STATION for  12 min ",
 		"<@rabot-test>  start  recording    ALPHA-STATION   1minute ",
 	})
 }
 
 func TestRemoveCommand(t *testing.T) {
-	app := &App{}
-	app.SetBotID("rabot-test")
-	_TestCommands(t, app.Commands.Remove, []string{
+	commands := &Commands{}
+	commands.SetBotID("rabot-test")
+	_TestCommands(t, commands.Remove, []string{
 		"<@rabot-test>  remove  container  foo  ",
 		"<@rabot-test>  rm  container  foo  ",
 		"<@rabot-test>  rm   foo  ",
@@ -44,9 +44,9 @@ func TestRemoveCommand(t *testing.T) {
 }
 
 func TestPingCommand(t *testing.T) {
-	app := &App{}
-	app.SetBotID("rabot-test")
-	_TestCommands(t, app.Commands.Ping, []string{
+	commands := &Commands{}
+	commands.SetBotID("rabot-test")
+	_TestCommands(t, commands.Ping, []string{
 		"<@rabot-test>  ping  ",
 	})
 }
